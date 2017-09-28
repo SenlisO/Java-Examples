246d9a1107224d0f74749e3b2a58c5c3f0f8dd1b
Richard Romick <gizmo.romick@gmail.com>
2017-09-28 23:43:35 +0900

    updated comments in example #1 for more readablity

--- 1_HelloWorld/HelloWorld.java
+++ 1_HelloWorld/HelloWorld.java
@@ -1,9 +1,9 @@
 //This is a comment.  It is not run by the computer, but rather is for humans to read
 //The "//" starting a line indicates it is a comment
 /*
- 	You can also define multiline
+ 	You can also create multiline
  	comments by using asterisks and forward slashes
- */
+*/
 
 /*
  	Every java program starts with a class with the same name as the java file.
@@ -12,7 +12,7 @@
  	will learn more about classes later.
  	The class also will have a method called "main".  Don't worry about what a method is right now,
  	just know that this marks where the computer will start executing code.
- */
+*/
 public class HelloWorld
 {
 	public static void main(String args[])
@@ -20,11 +20,11 @@ public class HelloWorld
 		//The computer starts executing code here
 		
 		/*
-		 	System.out is a tool we use to print (display) text to the screen.  The specific command is println or print line.
-		 	System.out.println takes what is in the parenthesis, prints (displays) it, and moves to a new line.
+		 	System.out is a tool we use to print (display) text to the screen.  The specific command is println or "print line".
+		 	System.out.println receives what is in parenthesis, prints (displays) it, and moves the cursor to a new line.
 		 */
 		System.out.println("Hello World");
-		//The double quotations indicates the contents are a String, or collection of characters.  We will get more into data
+		//The double quotations indicates the contents are a String (collection of characters).  We will get more into data
 		//types later
-	} //curley brackets are used to define blocks of code.  For right now, know that this curley bracket ends the program.
+	} //Curly brackets are used to define blocks of code.  For right now, know that this Curly bracket ends the program.
 }
