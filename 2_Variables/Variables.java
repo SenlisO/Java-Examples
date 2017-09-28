c6d6c4c1b46c6bcb42453cd446a98ade194df1e0
Richard Romick <gizmo.romick@gmail.com>
2017-09-28 23:49:38 +0900

    Updated comments in example #2 for clarity

--- 2_Variables/Variables.java
+++ 2_Variables/Variables.java
@@ -2,17 +2,16 @@ public class Variables
 {
 	public static void main(String args[])
 	{
-		//We aren't going to re-hash the comments from the Hello World example
+		//We aren't going to re-hash the same comments from the Hello World example, but rather cover new material
 		
-		//The following are variables.  You can think of them as placeholders in memory that
-		//have different data types (types of values)
+		//The following are variables.  You can think of them as storage that holds a specific data type
 		
 		int number = 1000; //This is an integer.  It stores whole numbers from -2,147,483,648 to 2,147,483,648
-		//That is 32 bits: 31 bits for the number and 1 bit for the positive or negative sign.
+		//Specifically, that number is 32 bits: 31 bits for the number itself and 1 bit for the positive or negative sign.
 		
-		double second_number = 1.25; //This is a double.  It stores numbers with a decimal point
+		double second_number = 1.25; //This is a double.  It stores numbers with a decimal point.
 		
-		char first_initial = 'R'; //This is a character.  It stores a single letter
+		char first_initial = 'R'; //This is a character.  It stores a single letter from a-z & A-Z
 		
 		String greeting = "Hello World"; //A string is a collection of characters
 		
@@ -24,15 +23,15 @@ public class Variables
 		
 		System.out.println(answer); //This should print 0
 		
-		answer = number + 150; //the '=' is an assignment operator.  It takes whatever the result is on the right
-		//and places the result in the variable to the left.
-		//Before, answer was 0.  After this operation, answer is now 1150.
+		answer = number + 150; //the '=' is an assignment operator.  It takes whatever the result is on the right of the '='
+		//and places it in the variable to the left. Before, answer was 0.  After this operation, answer is now 1150.
 		
-		System.out.println(answer); //Now it prints 1150
+		System.out.println(answer); //prints 1150
 		
 		/*
-		 note that '=' is not equality.  We are not saying that the left is equal to the right.  For that,
-		 we use '=='.  The result of '==' is a boolean, either true or false.  This is an example of a comparison statement
+		 note that '=' is not equality, it is assignment.  In other words, We are not saying that the left is equal to the right.  
+		 For equality, we use '=='.  The result of '==' is a boolean, either true or false.  
+		 '==' is one of multiple possible comparison statements.
 		*/
 		
 		System.out.println(answer == 1150); //This should print 'true'
