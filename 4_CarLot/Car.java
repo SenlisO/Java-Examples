06a90ef87b6d948a24b6d7d4e87b26cdc73ed23c
Senlis <gizmo.romick@gmail.com>
2017-10-02 20:45:50 -0800

    updated comments in example #4 for more clarity

--- 4_CarLot/Car.java
+++ 4_CarLot/Car.java
@@ -1,5 +1,13 @@
-public class Car
+/*
+ * Read comments in CarLot.java first
+ * 
+ */
+
+public class Car // This is where we define the Car class
 {
+	// Inside of the car class, we have several variables called attribute.  They store the state of the object
+	// These attributes are private, which means only code in the Car class can modify them.
+	// The advantage of this is that we can validate in the Car classes that attributes have valid values 
 	private int odometer;
 	private int fuel_in_miles;
 	private String make;
@@ -9,6 +17,10 @@ public class Car
 	
 	public Car(int year_input, String make_input, String model_input, String color_input)
 	{
+		// The method (function) that has the same name as the class is the initiator.  it is called when we created the
+		// my_car object in CarLot.java.
+		
+		// This if statement uses other methods in the car object to validate the parameters given have proper values.
 		if (valid_year(year_input) && valid_make(make_input) && valid_model(model_input) && valid_color(color_input))
 		{
 			make = make_input;  //valid input: Honda, Ford or Chevy
@@ -20,10 +32,11 @@ public class Car
 		}
 		else
 		{
-			throw new IllegalArgumentException("Vehlicle inputs are not valid");
-		}
+			throw new IllegalArgumentException("Vehlicle inputs are not valid"); //an exception is an error sent back to
+		}  // the function that called it.  An exception should be "caught", see the corresponding code in CarLot.java
 	}
 	
+	// These methods can be used to verify attributes maintain correct values
 	private boolean valid_make(String test_make)
 	{
 		if (test_make == "Honda" || test_make == "Ford" || test_make == "Chevy")
@@ -64,9 +77,11 @@ public class Car
 		return false;
 	}
 	
+	// The following methods are public, so anyone can use it.  The purposes of which should be readily apparent.
 	public void gas_up()
 	{
-		fuel_in_miles = 420;
+		fuel_in_miles = 420; //remember that fuel_in_miles belongs to this class, review the variables at the beggining
+		// of the class definition if necessary
 	}
 	
 	public boolean drive(int miles_to_drive)
@@ -78,13 +93,14 @@ public class Car
 			odometer = odometer + miles_to_drive;
 			return false;
 		}
-		//fuel_in_miles is negative
 		
+		//fuel_in_miles is negative
 		odometer = odometer + miles_to_drive + fuel_in_miles;
 		fuel_in_miles = 0;
 		return true; //we are out of gas!
 	}
 	
+	// since all the attributes are private, we need a way for the user interface to read the value of those attributes
 	public int get_odometer_reading()
 	{
 		return odometer;
--- 4_CarLot/CarLot.java
+++ 4_CarLot/CarLot.java
@@ -1,13 +1,24 @@
-public class CarLot
-{
+/*
+ * This example explains classes and objects
+ * We have been using classes the entire time have been using java.  Anything that is separated by a period is probably a class
+ * For example: System.out.println is from the class System.  This example will explain how we make our own classes.
+ */
+
+public class CarLot //This is the class that launches the program, as it has the 'main' function
+{ // Usually, these types of classes would be user interface classes
 	public static void main(String args[])
 	{
-		try
+		try // this try corresponds to the catch block below.  You will understand more after the next few lines.
 		{
-		
 			Car my_car = new Car(2004, "Honda", "Accord", "Silver");
-			boolean out_of_gas = false;
+			/* In the example above, we create a Car called my_car.  Car is a class (think template) I have defined in Car.java, 
+			 * and my_car is an object (think of a creation from the template).  I provided details in the parenthesis
+			 * to tell my new car object what it is.  Now, please go to Car.java.
+			 */
+			
+			boolean out_of_gas = false; //this is a local variable
 		
+			// now you can see our "User Interface class" calling methods from the Car object.
 			System.out.println(my_car.get_odometer_reading());
 			
 			my_car.gas_up();
@@ -25,12 +36,12 @@ public class CarLot
 			System.out.println(my_car.get_odometer_reading());
 			if (out_of_gas)
 			{
-				System.out.println("Goint to the gas station");
+				System.out.println("Going to the gas station");
 				my_car.gas_up();
 			}
 			
 		}
-		catch (IllegalArgumentException e)
+		catch (IllegalArgumentException e)  // this is the catch block for the Car initiator method
 		{
 			System.out.println("That is not a valid vehicle");
 		}
