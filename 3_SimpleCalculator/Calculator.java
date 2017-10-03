c16f51ad2deefe98d17be2eb7d9e778ba7c720c3
Senlis <gizmo.romick@gmail.com>
2017-10-02 20:27:21 -0800

    updated comments in example #3 for more clarity

--- 3_SimpleCalculator/Calculator.java
+++ 3_SimpleCalculator/Calculator.java
@@ -20,25 +20,26 @@ public class Calculator
 		
 		//Read the users first number
 		System.out.print("Enter your first number: ");  //this time we use "print" instead of "println" so the
-														//cursor does not go to the next line
-		first_number = input.nextDouble();  //receive the first number
+														//cursor does not move to the next line
+		first_number = input.nextDouble();  //receive the first number from user
 		
 		System.out.print("Enter your second number: ");
-		second_number = input.nextDouble(); //receive the second number
+		second_number = input.nextDouble(); //receive the second number from user
 		
 		System.out.print("Enter your operand ");
-		temp = input.next(); //receives user's data and store it in a temporary String
-		operand = temp.charAt(0); //retrieves the first character in the temporary variable and store it in variable operand
-		//we do this to ensure that only the first character the user enters is used
+		temp = input.next(); //receives the operand from the user and stores it in temporary variable
+		operand = temp.charAt(0); //retrieves the first character in the temporary variable and stores it in variable operand
+		//we do this to ensure that only the first character the user enters is examined
 				
 		//at this point, we have the users two numbers and their operand.  We need to decide which operation we need to
 		//perform on the two numbers.  To that end, we will use decision statements.
 		
 		if (operand == '+')  //The if statement evaluates the comparison in the parenthesis.  Note the double equal signs
-		{  //if true, the code inside the brackets is executed.  If false, the computer skips the coe inside the brackets
+		{   //If the comparison inside the parenthesis evaluates to 'true', the code inside the brackets is executed.  
+			//If 'false', the computer skips the code inside the brackets
 			answer = first_number + second_number;
 		}
-		else if (operand == '-')  //else if statements are only execute if the previous if/else if was false
+		else if (operand == '-')  //else if statements only execute if the previous if or else if was false
 		{
 			answer = first_number - second_number;
 		}
@@ -56,11 +57,11 @@ public class Calculator
 		}
 		else //code in an else block only executes if all previous if and else if statements evaluate to false
 		{
-			System.out.println("That isn't a valid operand you jerk!");
+			System.out.println("That isn't a valid operator you jerk!");
 		}
 		
-		System.out.println("Your answer is: " + answer);  //in this case, we will append the variable answer to the println using +
+		System.out.println("Your answer is: " + answer);  //in this case, we will append the variable answer to the println using '+'
 		
-		input.close();
+		input.close(); // We have to close the scanner object otherwise we leak memory
 	}
 }
